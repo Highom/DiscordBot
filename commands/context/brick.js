25f291f6eb42a513b78ce088e264a6fb51ed7489
Highom <highomlp@gmail.com>
2023-05-07 20:43:47 +0200

    Brick command is now hella janky :)

--- commands/context/brick.js
+++ commands/context/brick.js
@@ -1,6 +1,7 @@
+const Jimp = require('jimp');
 const Command = require('../../structures/CommandClass');
-
-const { EmbedBuilder, ApplicationCommandType, ContextMenuCommandBuilder } = require('discord.js');
+const { EmbedBuilder, AttachmentBuilder, ApplicationCommandType, ContextMenuCommandBuilder } = require('discord.js');
+const { GifFrame, GifUtil, GifCodec } = require('gifwrap');
 
 module.exports = class Brick extends Command {
 	constructor(client) {
@@ -15,16 +16,38 @@ module.exports = class Brick extends Command {
 			permissions: ['Use Application Commands', 'Send Messages', 'Embed Links'],
 		});
 	}
-	async run(client, interaction) {
+
+	async run(client, interaction) {		
+		const imageFolder = process.env.IMAGE_FOLDER;
+
 		const user = client.users.cache.get(interaction.targetId) || interaction.user;
+		
+		const readGif = await GifUtil.read(imageFolder + "\\brick.gif");
+		const promises = readGif.frames.map( async gifFrame => {
+			const jimpImage = GifUtil.copyAsJimp(Jimp, gifFrame);
 
-		//Do some stuff to the Avatar to Overlay a brick gif!
+			//get user image here
+			const userImg = await Jimp.read(imageFolder + "\\avatar.png");
+			jimpImage.composite(userImg, 0, 0)
+		  
+			GifUtil.quantizeDekker(jimpImage,256);
+			return new GifFrame(jimpImage.bitmap, {
+			  disposalMethod: gifFrame.disposalMethod, // not documented by gifwrap but it's in the source
+			  delayCentisecs: gifFrame.delayCentisecs, 
+			});
+		});
+		const gifFramesWithUser = await Promise.all(promises);
 
-		const embed = new EmbedBuilder()
-			.setTitle(`** ${interaction.user.username} throws a brick at ${user.username}**`)
-			.setColor(client.config.embedColor)
-			.setImage(user.displayAvatarURL({ size: 2048 }));
-
-		await interaction.reply({ embeds: [embed] });
-	}
-};
+		const codec = new GifCodec();
+		codec.encodeGif(gifFramesWithUser).then( async gif => {
+				const attachment = new AttachmentBuilder(gif.buffer, { name: 'brick.gif' });
+		
+				const embed = new EmbedBuilder()
+					.setTitle(`** ${interaction.user.username} throws a brick at ${user.username}**`)
+					.setColor(client.config.embedColor)
+					.setImage('attachment://brick.gif');
+			
+				await interaction.reply({ embeds: [embed], files: [attachment] });
+		});
+	};
+}
